9ca63f9fdace9bf91a494478c3cfd5b5502621be
whitneyseiler <whitney.seiler@gmail.com>
2018-01-31 19:55:58 -0800

    got some functionality

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,12 +1,12 @@
-var VideoList = () => (
-  <div className="video-list">
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-  </div>
-);
+var VideoList = (props) => {
+
+  return (
+    <div className="video-list-entry">
+      {props.videos.map((video, i) => <VideoListEntry video={video} key={i} /> )}
+    </div>
+  );
+};
+
 
 // PropTypes tell other developers what `props` a component expects
 // Warnings will be shown in the console when the defined rules are violated
@@ -17,3 +17,5 @@ VideoList.propTypes = {
 // In the ES6 spec, files are "modules" and do not share a top-level scope.
 // `var` declarations will only exist globally where explicitly defined.
 window.VideoList = VideoList;
+
+
